427b2842c1068294c0209e9bedb34e182fe64cbe
jaluik <cqzhengmouren@gmail.com>
2021-04-18 15:10:27 +0800

    feat: 增加types类型定义

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-const wechatAdapter = async (): Promise<boolean> => {
-  return true;
+import { AxiosAdapter, Method } from 'axios';
+import { WechatRequestMethod } from './global';
+
+const methodProcessor = (axiosMethod: Method): WechatRequestMethod => {
+  const upperCaseMethod = axiosMethod.toUpperCase();
+  const repeatMethod = ['GET', 'PUT', 'DELETE', 'OPTIONS', 'POST'];
+  if (repeatMethod.includes(upperCaseMethod)) {
+    return upperCaseMethod as WechatRequestMethod;
+  } else {
+    throw new Error(`微信小程序暂不支持${axiosMethod}方法`);
+  }
 };
+
+const wechatAdapter: AxiosAdapter = (config) => {
+  console.log('config', config);
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: config.url,
+      method: methodProcessor(config.method),
+      timeout: config.timeout,
+    });
+  });
+};
+
+export default wechatAdapter;
